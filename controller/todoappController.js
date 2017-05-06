4e9239ca22c9296683bd92fe79b13e207898f861
Sharad Jain <shjain@expedia.com>
2017-05-06 17:33:45 +0530

    moving connector to mongoose

--- controller/todoappController.js
+++ controller/todoappController.js
@@ -1,21 +1,38 @@
-var model = require("../model/tasks");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/todo');
+var Schema = mongoose.Schema
+var todo = new Schema({
+    task: {type: String, default: ''}
+});
+
+var todoModel = mongoose.model('tasks', todo);
 
 module.exports = function (app) {
 
     app.get("/todo", function (req, res) {
-        model.getTasks(function(tasks){
-            res.render("todoPage.ejs", tasks)
-        });
+        todoModel.find({}, function (err, docs) {
+            if(err) throw err;
+            var tasks = []
+            docs.forEach(function (currentDoc) {
+                tasks.push(currentDoc.task)
+            });
+            res.render("todoPage.ejs", {"tasks": tasks})
+        })
     });
 
     app.post("/todo", function (req, res) {
-        model.addTask(req.body);
-        res.redirect("/todo");
+        todoModel(req.body).save(function (err, result) {
+            if (err) throw err;
+            res.redirect("/todo");
+        })
     });
 
     app.delete("/tododelete/:task", function (req, res) {
-        model.removeTask(req.params.task,function(){
-            res.sendStatus(200)
+        todoModel.findOne({"task": req.params.task.split("-").join(" ")}, function (err, result) {
+            if(err) throw err;
+            result.remove(function (err, result) {
+                res.sendStatus(200);
+            });
         });
     });
 };
