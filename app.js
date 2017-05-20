83d35f994cc1499e3ee1c087a334144dda39ffd2
Sharad Jain <shjain@expedia.com>
2017-05-20 15:29:47 +0530

    changing port

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.static('./public'));
 //fire controllers
 controller(app);
 
-app.listen(3000, function () {
+app.listen(8080, function () {
     console.log('Example app listening on port 3000!')
 });
